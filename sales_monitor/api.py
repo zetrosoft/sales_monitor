a56827b1a7fa75c7d187eafabce8f17401612150
Bijak Technology <iswanputera@gmail.com>
2025-08-29 22:54:27 +0700

    update whitelist api get employee

--- sales_monitor/api.py
+++ sales_monitor/api.py
@@ -242,7 +242,7 @@ def get_order_history(store_name):
         frappe.log_error(frappe.get_traceback(), "Error in get_order_history")
         frappe.throw(f"Failed to fetch order history: {e}")
 
-@frappe.whitelist()
+@frappe.whitelist(allow_guest=False, allow_roles=["Sales User"])
 def get_employee_id(user_id):
     try:
         employee = frappe.db.get_value("Employee", {"user_id": user_id}, "name")
